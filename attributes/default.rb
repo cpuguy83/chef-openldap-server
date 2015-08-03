e402ce863e29b4fb12d1009c7951905aaf50d465
Chris Gianelloni <chris@cask.co>
2015-08-03 16:33:06 -0700

    Update attributes for default_config
    - Change attribute names to match variable names
    - Fix template for slapd_conf_file -> slapd_conf
    - Make sure updates are backwards-compatible

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,10 +4,23 @@ default['openldap-server'][:default_config][:slapd_user] = 'openldap' # SLAPD_US
 default['openldap-server'][:default_config][:slapd_group] = 'openldap' # SLAPD_GROUP
 default['openldap-server'][:default_config][:slapd_pidfile] = nil # SLAPD_PIDFILE
 default['openldap-server'][:default_config][:slapd_services] = "ldap:/// ldapi:///" # SLAPD_SERVICES
-default['openldap-server'][:default_config][:slapd_nostart] = false # SLAPD_NO_START
 default['openldap-server'][:default_config][:slapd_sentinel_file] = '/etc/ldap/noslapd' # SLAPD_SENTINEL_FILE
-default['openldap-server'][:default_config][:slapd_kerb_file] = '/etc/krb5.keytab' # KRB_KTNAME
 default['openldap-server'][:default_config][:slapd_options] = nil # SLAPD_OPTIONS
+
+# These attributes were not originally 1:1 with variable names, so preserve them for compatibility
+default['openldap-server'][:defaut_config][:slapd_no_start] =
+  if node['openldap-server'][:default_config].key?(:slapd_nostart)
+    node['openldap-server'][:default_config][:slapd_nostart]
+  else
+    false
+  end # SLAPD_NO_START
+
+default['openldap-server'][:default_config][:krb5_ktname] =
+  if node['openldap-server'][:default_config].key?(:slapd_kerb_file)
+    node['openldap-server'][:default_config][:slapd_kerb_file]
+  else
+    '/etc/krb5.keytab'
+  end # KRB5_KTNAME
 
 default['openldap-server'][:domain] = 'default.com'
 default['openldap-server'][:rootpw] = '{SSHA}0NDdeikW2csDSgF4+It1SLhpJYw3d6+z'
